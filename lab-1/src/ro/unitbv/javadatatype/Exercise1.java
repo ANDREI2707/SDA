24310fe2a1a09279c5c8ed6ec6b451aecdcc96d6
ANDREI2707 <adi.ciocoiu27@yahoo.com>
2020-03-04 19:01:58 +0200

    ex1

--- lab-1/src/ro/unitbv/javadatatype/Exercise1.java
+++ lab-1/src/ro/unitbv/javadatatype/Exercise1.java
@@ -1,10 +1,37 @@
 package ro.unitbv.javadatatype;
+import java.util.Scanner;
 
 public class Exercise1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("Hello world");
-	}
+     System.out.println("Hello world");
+     
+     Scanner scanner=new Scanner(System.in);
+     System.out.println("Introdu nr 1: ");
+     int nr1=scanner.nextInt();
+     //int nr2=scanner.nextInt();
+     
+     //afisam output-ul
+     System.out.println("numarul citit este:"+nr1);
+     System.out.println("Introdu nr 2: ");
+     
+     int nr2=scanner.nextInt();
+     System.out.println("numarul 2 citit este: "+nr2);
+     int suma=nr1+nr2;
+     System.out.println("suma este: "+suma);
+     System.out.println("diferenta este: " +(nr1-nr2));
+     System.out.println("produsul este: "+(nr1*nr2));
+     //nr1/nr3 afisat cu zecimale
+     System.out.println("media este: "+((nr1+nr2)/2));
+     if(nr1>nr2)
+    	 System.out.println("Distanta este "+(nr1+nr2)/2);
+     System.out.println("distanta intre 2 intregi este ");
+     if(nr1>nr2)
+    	 System.out.println("intregul maxim este:"+nr1+"(Nr 1)");
+     else
+    	 System.out.println("intregul maxim este: "+nr2+"(Nr 2");
+     
+     }
 
 }
